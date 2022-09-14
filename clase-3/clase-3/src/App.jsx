b7d0517f867519430d576abe6b0a67e5004defe0
Francisco Pugh <franciscopugh01@gmail.com>
2022-09-13 21:21:44 -0300

    Clase 3

---
+++ clase-3/clase-3/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+    return (
+        <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon" />
+                </button>
+            <div className="collapse navbar-collapse" id="navbarColor01">
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">Home
+            <span className="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div className="dropdown-menu">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <a className="dropdown-item" href="#">Something else here</a>
+            <div className="dropdown-divider" />
+            <a className="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-sm-2" type="text" placeholder="Search" />
+        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+        </div>
+        </div>
+        </nav>
+        
+    </>
+    );
+}
+
+export default App;
